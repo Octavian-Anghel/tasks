af1244b818f11647b10c5e06e5ab006506213ea1
Octavian Anghel <octavian.anghel@maine.edu>
2023-09-11 16:41:03 -0400

    Write isQuestion function

--- src/functions.ts
+++ src/functions.ts
@@ -41,7 +41,8 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    const lastCharacter = message.slice(-1);
+    return lastCharacter === "?";
 }
 
 /**
