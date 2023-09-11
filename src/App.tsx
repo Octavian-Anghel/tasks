d4ff76a0f95437328e0ed4a6e161ab71b433528d
Octavian Anghel <octavian.anghel@maine.edu>
2023-09-11 16:00:31 -0400

    Add HTML/CSS requirements for assignment

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import img from "./images/image.jpg";
 
 function App(): JSX.Element {
     return (
@@ -7,6 +9,58 @@ function App(): JSX.Element {
             <header className="App-header">
                 UM COS420 with React Hooks and TypeScript
             </header>
+            <h1 style={{ backgroundColor: "rgb(40, 44, 52)" }}>
+                This is a header
+            </h1>
+            <img src={img} alt="Walking Cat" width="200" height="133.2" />
+            <ul>
+                <li> 1st Element</li>
+                <li> 2nd Element</li>
+                <li> 3rd Element</li>
+            </ul>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
+            <Container>
+                <Row>
+                    <Col>
+                        <div
+                            style={{
+                                display: "flex",
+                                flexDirection: "column",
+                                alignItems: "center"
+                            }}
+                        >
+                            <p>First column</p>
+                            <div
+                                style={{
+                                    width: "200px",
+                                    height: "100px",
+                                    backgroundColor: "red"
+                                }}
+                            ></div>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div
+                            style={{
+                                display: "flex",
+                                flexDirection: "column",
+                                alignItems: "center"
+                            }}
+                        >
+                            <p>Second column</p>
+                            <div
+                                style={{
+                                    width: "200px",
+                                    height: "100px",
+                                    backgroundColor: "red"
+                                }}
+                            ></div>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
