2a67f2fc854b67532994d2de6f828dbf37bfa8d2
Octavian Anghel <octavian.anghel@maine.edu>
2023-09-17 20:19:59 -0400

    write allRGB function

--- src/arrays.ts
+++ src/arrays.ts
@@ -81,7 +81,13 @@ export function countShortWords(words: string[]): number {
  * then return true.
  */
 export function allRGB(colors: string[]): boolean {
-    return false;
+    if (colors.length === 0) {
+        return true;
+    }
+    const isRGB = (color: string): boolean =>
+        color === "red" || color === "blue" || color === "green";
+
+    return colors.every(isRGB);
 }
 
 /**
