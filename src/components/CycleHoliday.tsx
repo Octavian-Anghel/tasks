1b3fa5d72a3fd87e4cd31d28219642e4ac709e91
Octavian Anghel <octavian.anghel@maine.edu>
2023-10-10 22:07:19 -0400

    write CycleHoliday component, remove htmlcss test

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,5 +2,26 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const holidays = ["🎃", "🎏", "🪔", "🦃", "🎄"];
+    const [currentHolidayIndex, setCurrentHolidayIndex] = useState(0);
+
+    const advanceByAlphabet = () => {
+        const nextIndex = (currentHolidayIndex + 1) % holidays.length;
+        setCurrentHolidayIndex(nextIndex);
+    };
+
+    const advanceByYear = () => {
+        const nextIndex = (currentHolidayIndex + 1) % holidays.length;
+        setCurrentHolidayIndex((currentHolidayIndex + 1) % holidays.length);
+    };
+
+    const currentHoliday = holidays[currentHolidayIndex];
+
+    return (
+        <div>
+            <div>Holiday: {currentHoliday}</div>
+            <Button onClick={advanceByAlphabet}>Advance by Alphabet</Button>
+            <Button onClick={advanceByYear}>Advance by Year</Button>
+        </div>
+    );
 }
