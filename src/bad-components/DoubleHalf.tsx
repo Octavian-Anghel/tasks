137eb63499ca9ef18d45dd42d568c9344a240af2
Octavian Anghel <octavian.anghel@maine.edu>
2023-10-17 21:55:39 -0400

    fix DoubleHalf component

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -2,23 +2,30 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import { dhValue, setDhValue } from "./DoubleHalfState";
 
-function Doubler(): JSX.Element {
-    return <Button onClick={() => setDhValue(2 * dhValue)}>Double</Button>;
+function Doubler({ value, setValue }: ChildProps): JSX.Element {
+    return <Button onClick={() => setValue(2 * value)}>Double</Button>;
 }
 
-function Halver(): JSX.Element {
-    return <Button onClick={() => setDhValue(0.5 * dhValue)}>Halve</Button>;
+function Halver({ value, setValue }: ChildProps): JSX.Element {
+    return <Button onClick={() => setValue(0.5 * value)}>Halve</Button>;
+}
+
+interface ChildProps {
+    value: number;
+    setValue: (value: number) => void;
 }
 
 export function DoubleHalf(): JSX.Element {
+    const [dhValue, setDhValue] = useState(1);
+
     return (
         <div>
             <h3>Double Half</h3>
             <div>
                 The current value is: <span>{dhValue}</span>
             </div>
-            <Doubler></Doubler>
-            <Halver></Halver>
+            <Doubler value={dhValue} setValue={setDhValue} />
+            <Halver value={dhValue} setValue={setDhValue} />
         </div>
     );
 }
