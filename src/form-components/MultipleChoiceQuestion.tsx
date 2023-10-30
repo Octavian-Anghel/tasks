9e44128f5162fe837a7865847e9cbdce73bf084a
Octavian Anghel <octavian.anghel@maine.edu>
2023-10-30 18:15:58 -0400

    create MultipleChoiceQuestion component

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -7,9 +7,27 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    // state
+    const [selectedChoice, setSelectedChoice] = useState(options[0]);
+
+    // control
+    const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedChoice(e.target.value);
+    };
+
+    const isCorrect = selectedChoice === expectedAnswer;
+    // view
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <select value={selectedChoice} onChange={handleChange}>
+                {options.map((option) => (
+                    <option key={option} value={option}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+            <div>Result: {isCorrect ? "✔️" : "❌"}</div>
         </div>
     );
 }
